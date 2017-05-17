ba4bd36ad6c8f52a511f1176a7d9a500bc0299d0
Denys Holovko <denys.holovko@ukr.net>
2017-05-18 01:25:40 +0300

    Changed some values

--- js.js
+++ js.js
@@ -2,5 +2,5 @@
 var name = ('Johny');
 var time = ('4:20');
 var place = ('Amsterdam');
-var event1 = prompt ('What event?', "Cannabis Fest");
-alert(name+ ' has a trip today at ' +time+ ' to ' +place+ ' for a ' +event1)
+var event1 = prompt ('Where?', "Cannabis Fest");
+alert(name+ ' has any deals at ' +time+ ' on the ' +event1+ ' today. ' );
