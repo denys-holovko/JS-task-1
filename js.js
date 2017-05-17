39f8a054c64d566617d8a7aaf0e7d350f84bc07e
Denys Holovko <denys.holovko@ukr.net>
2017-05-18 00:26:42 +0300

    Added new files

---
+++ js.js
@@ -0,0 +1,6 @@
+
+var name = ('Johny');
+var time = ('4:20');
+var place = ('Amsterdam');
+var event1 = prompt ('What event?', "Cannabis Fest");
+alert(name+ ' has a trip today at ' +time+ ' to ' +place+ ' for a ' +event1)
